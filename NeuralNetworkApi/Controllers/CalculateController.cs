50a2a7c044bd5b8456a99079b41f5a1382a23d90
nioch <nioche.hippolyte@gmail.com>
2022-10-03 23:51:56 +0200

    Ajout du controlleur de calcule

---
+++ NeuralNetworkApi/Controllers/CalculateController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Useful_training.Core.Neural_network;
+using Useful_training.Core.Neural_network.Interface;
+using Useful_training.Core.Neural_network.ValueObject;
+
+namespace NeuralNetworkApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]/")]
+    public class CalculateController : ControllerBase
+    {
+        private readonly INeuralNetworkWarehouse NeuralNetworkWarehouse;
+        public CalculateController(INeuralNetworkWarehouse neuralNetworkWarehouse)
+        {
+            NeuralNetworkWarehouse = neuralNetworkWarehouse;
+        }
+
+        [HttpGet("{Name}/{Input}", Name = "GetCalculationResult")]
+        public IEnumerable<double> Get(string Name, [FromQuery] List<double> Inputs)
+        {
+            return NeuralNetworkWarehouse.Retreive<NeuralNetwork>(Name).Calculate(Inputs);
+        }
+
+    }
+}
